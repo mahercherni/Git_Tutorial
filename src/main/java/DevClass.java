a4d8289bfd1dd142c939c04e97cf30b3f90d3adb
mahercherni <96537858+mahercherni@users.noreply.github.com>
2021-12-22 16:56:09 +0100

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -3,6 +3,7 @@ public class DevClass {
 
     public static void main(String[] args) {
 
-        System.out.println("Dev Class");
+        System.out.println("Dev Class +Maher Cherni");
+        // checking from maherchernibranch
     }
 }
